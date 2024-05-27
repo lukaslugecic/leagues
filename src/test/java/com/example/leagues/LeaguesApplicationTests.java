1792e0e057aaa5fdb6e6949ba17c8da5624b7904
Luka <luka.slugecic@gmail.com>
2024-05-27 17:53:17 +0200

    Initial commit

---
+++ src/test/java/com/example/leagues/LeaguesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leagues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaguesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
